f61cd03fbed700261480d74b108e24d314784d57
OM26232 <sjacobs4@oldmutual.com>
2025-04-15 14:56:01 +0200

    changes to Main.tf

--- infrastructure-as-code/main.tf
+++ infrastructure-as-code/main.tf
@@ -2,7 +2,7 @@ variable "files" {
      default = [
        "file0.txt",  # index 0
        null,         # index 1 - delete record
-       null,         # index 2
+       "file2txt",   # index 2
        "file3.txt",  # index 3
        "file4.txt"   # index 4
      ]
@@ -10,7 +10,7 @@ variable "files" {
  
  
  
-resource "local_file" "foo" {
+resource "local_file" "foo1" {
   for_each = {
     for idx, filename in var.files : idx => filename
     if filename != null
